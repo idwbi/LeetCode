67b5a2a8fb1737c2c1afadb9746f52379877c3fb
idwbi <33658158+idwbi@users.noreply.github.com>
2017-11-26 17:58:21 +0900

    Reverse Words in a String
    Given an input string, reverse the string word by word.
    
    For example,
    Given s = "the sky is blue",
    return "blue is sky the".

---
+++ reverseWords.java
@@ -0,0 +1,11 @@
+//Array/String互轉: TC:　O(n), SC: O(n)
+public class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split("\\s+");
+        String ans = "";
+        for (int i = words.length - 1; i >= 0; i--) {
+            ans += words[i] + " ";
+        }
+        return ans.trim();
+    }
+}
